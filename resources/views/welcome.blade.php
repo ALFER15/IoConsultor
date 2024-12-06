d9ee0757c0e3c9d23cacecd6224d2b3d6820980b
ALFER15 <aarias22@alumnos.uaq.mx>
2024-12-06 02:11:54 -0600

    Modificacion de colores en la vista welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,9 +13,9 @@
         <div class="md:w-1/2 space-y-4">
             <a
                                         href="{{ route('login') }}"
-                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                    >
-                                        Entra a la web
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-[#EF4444] focus:outline-none focus-visible:ring-[#FF2D20] dark:text-dark dark:hover:text-dark/80 dark:focus-visible:ring-white"
+                                        >
+                                        Acceder
                                     </a>
             <span class="block bg-black text-white px-4 py-1 text-sm font-semibold rounded-full">
                 IoConsultor
